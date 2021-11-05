32996ba82f64fa55d69a2a32f31eb956d9e15f02
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-11-05 17:18:11 +0100

    correct two bugs

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
+        //first bug
+        //this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
+        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
         this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
 
         this.configureTextViewMain();
@@ -29,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private void configureTextViewQuantity(){
         int quantity = Utils.convertDollarToEuro(100);
         this.textViewQuantity.setTextSize(20);
-        this.textViewQuantity.setText(quantity);
+        //second bug
+        //this.textViewQuantity.setText(quantity);
+        this.textViewQuantity.setText(String.valueOf(quantity));
     }
 }
