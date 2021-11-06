a28d8e8045c801c1f6d72cc0a81cd874ff0cc7f3
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-11-06 15:38:36 +0100

    create models.RealEstate add Hilt

--- app/src/main/java/com/openclassrooms/realestatemanager/MainApplication.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainApplication.kt
@@ -1,9 +1,10 @@
 package com.openclassrooms.realestatemanager
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 
-
+@HiltAndroidApp
 class MainApplication : Application() {
 
 
