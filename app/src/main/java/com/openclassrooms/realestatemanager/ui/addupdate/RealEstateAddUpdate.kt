d565e897a1e1d81915ff3663f100e23334fb1571
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-11-19 13:25:23 +0100

    add @Embedded to room for address and photo

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/addupdate/RealEstateAddUpdate.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/addupdate/RealEstateAddUpdate.kt
@@ -1,25 +1,18 @@
 package com.openclassrooms.realestatemanager.ui.addupdate
 
 import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getExternalFilesDirs
-import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -30,6 +23,8 @@ import com.openclassrooms.realestatemanager.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.File
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,7 +44,7 @@ class RealEstateModifier : Fragment() {
     private val binding get() = _binding!!
 
 
-    lateinit var  activityResultLauncher: ActivityResultLauncher<Intent>
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     //viewmodel
     private val mainViewModel by viewModels<MainViewModel>()
@@ -75,69 +70,50 @@ class RealEstateModifier : Fragment() {
 
         val rootView = binding.root
 
-
         //bind recyclerview
         val recyclerView: RecyclerView = binding.recyclerview
 
-
-        val getImage = registerForActivityResult(
+        //to get image from gallery
+        val getImageFromGallery = registerForActivityResult(
             ActivityResultContracts.GetContent(),
-            ActivityResultCallback {
-                binding.imageOfGallery?.setImageURI(it)
-            }
+            ActivityResultCallback {binding.imageOfGallery?.setImageURI(it)}
         )
 
-
-
-        val getPhoto = registerForActivityResult(
+        //to get image from camera
+        val getPhotoFromCamera = registerForActivityResult(
             ActivityResultContracts.TakePicture(),
-            ActivityResultCallback {it
-
-               // binding.imageOfGallery?.setImageURI(it)
-            }
+            ActivityResultCallback {it}
         )
 
-
-
-
-
-        //from camera
+        //listener for camera
         binding.addPhotoCamera?.setOnClickListener {
-
             var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             activityResultLauncher.launch(intent)
-
-
         }
 
-        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result: ActivityResult? ->
-            if (result?.resultCode==Activity.RESULT_OK)
-            {
-                var bitmap = result!!.data!!.extras!!.get("data") as Bitmap
-                binding.imageOfGallery.setImageBitmap(bitmap)
-            }
-            else
-            {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+                if (result?.resultCode == Activity.RESULT_OK) {
+                    var bitmap = result!!.data!!.extras!!.get("data") as Bitmap
+                    binding.imageOfGallery.setImageBitmap(bitmap)
 
+                    val fileName: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                    //val storageDir = File(context?.filesDir, "test")
+                    savePhotoToInternalMemory("Photo_$fileName", bitmap)
+                }
             }
-        }
 
-        //from galery
+        //from gallery
         binding.addPhotoFromMemory?.setOnClickListener {
-
-            getImage.launch("image/*")
+            getImageFromGallery.launch("image/*")
             // setupGetPhotoFromGalery()
             Log.i("[THOMAS]", "test ouverture photo")
         }
 
-
-
-
+        //insert data into database
         binding.saveBtn?.setOnClickListener {
             mainViewModel.insert(RealEstate(price = 1000))
         }
-
 
         //for test
         mainViewModel.allRealEstate.observe(viewLifecycleOwner) { listRealEstate ->
@@ -146,67 +122,11 @@ class RealEstateModifier : Fragment() {
             setupRecyclerView(recyclerView, listRealEstate)
         }
 
-
-
-
         return rootView
     }
 
 
-
-
-
-    private fun setupGetPhotoFromGalery() {
-
-        var intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, 456)
-
-
-    }
-
-
-    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-
-        if (requestCode == 456) {
-            binding.imageOfGallery?.setImageURI(data?.data)
-
-            Log.i("[THOMAS]", "URI image from galery : $data")
-
-        }
-
-        if (requestCode == 457) {
-            Log.i("[THOMAS]","Take a photo : " + data.toString())
-
-            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
-                val imageBitmap = data?.extras!!.get("data") as Bitmap
-                binding.imageOfGallery?.setImageBitmap(imageBitmap)
-
-
-
-                val fileName:String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-                val storageDir = File(context?.filesDir, "test")
-
-                //   val photoUri : Uri = FileProvider.getUriForFile(this, "com.openclassrooms.realestatemanager",storageDir)
-
-                //File.createTempFile("JPEG_THOMAS_",".jpg",context?.filesDir).apply { Log.i("[THOMAS]", "Photo path :$absolutePath"  ) }
-
-                savePhotoToInternalMemory("Photo_$fileName",imageBitmap)
-
-
-            }
-
-        }
-
-
-
-    }
-
-     */
-
-
+    //TODO: store outside function
     private fun savePhotoToInternalMemory(filename: String, bmp: Bitmap): Boolean {
         return try {
             context?.openFileOutput("$filename.jpg", Activity.MODE_PRIVATE).use { stream ->
@@ -216,9 +136,7 @@ class RealEstateModifier : Fragment() {
                     throw IOException("erreur compression")
                 }
 
-
-                //  FileProvider.getUriForFile(requireContext(),"com.openclassrooms.realestatemanager.fileprovider",$filename)
-                Log.i("[THOMAS]", "chemin " + context?.filesDir)
+              //  Log.i("[THOMAS]", "chemin " + context?.filesDir)
 
             }
             true
@@ -230,10 +148,10 @@ class RealEstateModifier : Fragment() {
         }
     }
 
+    //to setup recyclerview
     private fun setupRecyclerView(
         recyclerView: RecyclerView,
         myRealEstateList: List<RealEstate>,
-        //   onClickListener: View.OnClickListener
     ) {
 
 
