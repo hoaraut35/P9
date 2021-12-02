459c0032c2c8266f913ff7ca3b85bddef01517f3
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-12-02 16:34:03 +0100

    add fragment_maps.xml and map get last position good

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/map/MapsFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/map/MapsFragment.kt
@@ -12,20 +12,33 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.FragmentMapsBinding
+import com.openclassrooms.realestatemanager.ui.create.ViewModelForCreate
+import dagger.hilt.android.AndroidEntryPoint
+import com.google.android.gms.maps.model.CameraPosition
 
+
+
+
+@AndroidEntryPoint
 class MapsFragment : Fragment() {
 
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModelMap by viewModels<ViewModelMap>()
+
+    lateinit var myGoogleMap : GoogleMap
 
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
@@ -39,6 +52,9 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+
+        myGoogleMap = googleMap
+
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
@@ -57,33 +73,41 @@ class MapsFragment : Fragment() {
         _binding = FragmentMapsBinding.inflate(inflater, container, false)
         val rootView = binding.root
 
-        val locationPermissionRequest = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permissions ->
-            when {
-                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                    // Precise location access granted.
-                    Log.i("[POSITION]", "Position autorisée")
-                }
-                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                    // Only approximate location access granted.
-                    Log.i("[POSITION]", "Position approximative")
-                }
-                else -> {
-                    // No location access granted.
-                    Log.i("[POSITION]", "Position refusée ")
-                }
-            }
-        }
-
-        locationPermissionRequest.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION))
+//        val locationPermissionRequest = registerForActivityResult(
+//            ActivityResultContracts.RequestMultiplePermissions()
+//        ) { permissions ->
+//            when {
+//                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+//                    // Precise location access granted.
+//                    Log.i("[POSITION]", "Position autorisée")
+//                }
+//                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+//                    // Only approximate location access granted.
+//                    Log.i("[POSITION]", "Position approximative")
+//                }
+//                else -> {
+//                    // No location access granted.
+//                    Log.i("[POSITION]", "Position refusée ")
+//                }
+//            }
+//        }
+//
+//        locationPermissionRequest.launch(arrayOf(
+//            Manifest.permission.ACCESS_FINE_LOCATION,
+//            Manifest.permission.ACCESS_COARSE_LOCATION))
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
             Log.i("[POSITION]", "Position " + location?.longitude + " " + location?.latitude)
+
+            if (location != null){
+                viewModelMap.location = location
+
+
+                setupMyLocation(  viewModelMap.setLocation(location))
+
+            }
         }
 
 
@@ -92,6 +116,24 @@ class MapsFragment : Fragment() {
 
         return rootView
     }
+
+    private fun setupMyLocation(latLng: LatLng) {
+        myGoogleMap.clear()
+        myGoogleMap.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title("My position")
+        )
+        val cameraPosition =
+            CameraPosition.Builder().target(latLng)
+                .zoom(14f).tilt(30f).bearing(0f).build()
+        myGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+
+        //myGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //myGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(13f));
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/map/ViewModelMap.kt
@@ -0,0 +1,20 @@
+package com.openclassrooms.realestatemanager.ui.map
+
+import android.location.Location
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.openclassrooms.realestatemanager.repositories.LocalDatabaseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ViewModelMap @Inject constructor(private val localDatabaseRepository: LocalDatabaseRepository): ViewModel() {
+
+    var location : Location? = null
+
+    fun setLocation(location: Location):LatLng {
+        return LatLng(location.latitude, location.longitude)
+    }
+
+
+}
